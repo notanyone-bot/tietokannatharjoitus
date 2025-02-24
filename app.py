a18a03bced02e45e629cb1d7b8a7a4269da85ed3
Alexey Pavlov <alexey.a.pavlov@utu.fi>
2025-02-24 12:38:03 +0200

    Add user pages

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import sqlite3
 from flask import Flask
 from flask import abort, redirect, render_template, request, session
-from werkzeug.security import check_password_hash, generate_password_hash
 import db
 import config
 import reservations
@@ -129,6 +128,12 @@ def create():
     password2 = request.form["password2"]
     if password1 != password2:
         return "VIRHE: salasanat eivät ole samat"
+    
+    try:
+        users.create_user(username, password1)
+    except sqlite3.IntegrityError:
+        return "VIRHE: tunnus on jo varattu"
+    return "Tunnus luotu"
     password_hash = generate_password_hash(password1)
 
     try:
@@ -142,17 +147,13 @@ def create():
 def login():
     if request.method == "GET":
         return render_template("login.html")
+    
     if request.method == "POST":
-
             username = request.form["username"]
             password = request.form["password"]
 
-            sql = "SELECT id, password_hash FROM users WHERE username = ?"
-            result = db.query(sql, [username])[0]
-            user_id = result["id"]
-            password_hash = result["password_hash"]
-
-            if check_password_hash(password_hash, password):
+            user_id = users.check_login(username, password)
+            if user_id:
                 session["user_id"] = user_id
                 session["username"] = username
                 return redirect("/")
